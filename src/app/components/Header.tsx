94c1da8079cfe9d7db6ce0d5576d74b03e9e4a5c
brysonmk <brysonkushner@google.com>
2025-12-30 17:21:46 -0500

    Updated layout and created header section

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <div className="flex justify-around">
+            <div><a href="/">Home</a></div>
+            <div><a href="/about">About</a></div>
+        </div>
+    )
+}
