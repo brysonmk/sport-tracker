6e79ea264ac026686f4768830a7a7f18f7cb8e22
brysonmk <brysonkushner@google.com>
2025-12-31 20:12:24 -0500

    Added contact section and updated header styling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,11 @@
 export default function Header () {
     return (
-        <div className="flex justify-around">
+        <div className="flex justify-center gap-10 py-6">
             <div><a href="/">Home</a></div>
-            <div><a href="/about">About</a></div>
+            <div className="flex direction-row gap-4">
+                <div><a href="/about">About</a></div>
+                <div><a href="/contact">Contact</a></div>
+            </div>
         </div>
     )
 }
