30facaeea851da8278788328f0d31288b87f663f
brysonmk <brysonkushner@gmail.com>
2025-12-31 22:48:22 -0500

    Setup format

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,13 @@
 export default function Header () {
     return (
-        <div className="flex justify-center gap-10 py-6">
+        <div className="flex justify-center gap-20 py-6">
             <div><a href="/">Home</a></div>
-            <div className="flex direction-row gap-4">
+            <div className="flex direction-row gap-6">
                 <div><a href="/about">About</a></div>
                 <div><a href="/contact">Contact</a></div>
+                <div><a href="/matches-table">Matches Table</a></div>
             </div>
+            <div><a href="/login">Login</a></div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        Sports Tracker App
+    <div>
+      <main>
+        <div>Sports Tracker App</div>
+        <div>
+          Landing Page OR...
+        </div>
+        <div>
+          Personal Feed
+        </div>
       </main>
     </div>
   );
