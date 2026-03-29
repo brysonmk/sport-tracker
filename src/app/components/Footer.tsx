5a5ed3269d6627b2f83487a7a35d8d9265aacedf
brysonmk <brysonkushner@gmail.com>
2026-03-29 01:30:25 -0400

    Implemented footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer () {
+    return (
+        <footer className="fixed bottom-0 left-0 right-0 bg-black border-t border-slate-200 text-center py-3 text-sm text-white">
+            <div>© {new Date().getFullYear()} Sports Tracker</div>
+        </footer>
+    )
+}   
